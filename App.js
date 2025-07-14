b468eddec0b11c38fbbf9b7b5a049d0499f271e5
phonglamm <lamphong1507@gmail.com>
2025-07-14 13:28:24 +0700

    init

--- App.js
+++ App.js
@@ -4,8 +4,18 @@ import { View } from "react-native";
 import Toast from "react-native-toast-message";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { CartProvider } from "./context/CartContext";
+import { Purchases } from "react-native-purchases";
 
 export default function App() {
+  useEffect(() => {
+    const initializeRevenueCat = async () => {
+      Purchases.configure({
+        apiKey: process.env.EXPO_PUBLIC_REVENUE_CAT_APPLE,
+      });
+    };
+    initializeRevenueCat();
+  }, []);
+
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <SafeAreaProvider>
